49eb66b33aedf807b4a30f98bd830c1b731effe5
Andersson Velasquez Alvites <cxasper23@gmail.com>
2022-11-25 04:23:25 -0500

    [UPDATE] script and refactor

--- kombat.py
+++ kombat.py
@@ -2,10 +2,10 @@ import json
 
 def who_initiates(secuence):
     count_mov_p1 = len([mov for mov in secuence['player1']['movimientos'] if mov != ''])
-    count_str_p1 = len([mov for mov in secuence['player1']['golpes'] if mov != ''])
+    count_str_p1 = len([str for str in secuence['player1']['golpes'] if str != ''])
 
     count_mov_p2 = len([mov for mov in secuence['player2']['movimientos'] if mov != ''])
-    count_str_p2 = len([mov for mov in secuence['player2']['golpes'] if mov != ''])
+    count_str_p2 = len([str for str in secuence['player2']['golpes'] if str != ''])
 
     total_p1 = count_mov_p1 + count_str_p1
     total_p2 = count_mov_p2 + count_str_p2
@@ -17,22 +17,44 @@ def who_initiates(secuence):
 
     return 'player1', 'player2'
 
+
 def movement_special(combination, skills):
     for skill in skills.keys():
         if combination.endswith(skill):
             return skills[skill]
 
-def printer_events(secuence: list):
 
+def printer_events(movement, hit, skills):
+    movement = movement
+    hit = hit
+    special_hit =  movement_special(movement + hit, skills['special'])
+    points = 0
+
+    if special_hit:
+        points = special_hit[1]
+        print(f"{skills['name']} conecta un {special_hit[0]}")
+    if not hit:
+        print(f"{skills['name']} se mueve")
+    if hit and not movement:
+        points = 1
+        print(f"{skills['name']} lanza una {skills['basic'][hit][0]}")
+    if not special_hit and hit and movement:
+        points = 1
+        print(f"{skills['name']} se mueve y lanza un(a) {skills['basic'][hit][0]}")
+
+    return points
+
+
+def talana_kombat(secuence: list):
     try:
         secuence = json.loads(secuence)
-        # secuence = {"player1":{"movimientos":["D","DSD","S","DSD","SD"],"golpes":["K","P","","K","P"]},"player2": {"movimientos":["SA","SA","SA","ASA","SA"],"golpes":["K","","K","P","P"]}}
     except Exception as e:
         raise ValueError('Ingrese un valor que con la sgte estructura: {"player1":{"movimientos":["D","DSD","S","DSD","SD"],"golpes":["K","P","","K","P"]},"player2": {"movimientos":["SA","SA","SA","ASA","SA"],"golpes":["K","","K","P","P"]}}')
 
     skills = {
         'player1': {
             'name': 'Tonyn',
+            'points': 5,
             'special': {
                 'DSDP': ['Taladoken', 3],
                 'SDK': ['Remuyuken', 2]
@@ -44,6 +66,7 @@ def printer_events(secuence: list):
         },
         'player2': {
             'name': 'Arnaldor',
+            'points': 5,
             'special': {
                 'SAK': ['Remuyuken', 3],
                 'ASAP': ['Taladoken', 2]
@@ -55,51 +78,34 @@ def printer_events(secuence: list):
         }
     }
 
-    points_p1 = 5
-    points_p2 = 5
-
     init_player, end_player = who_initiates(secuence)
 
-    for index, movement in enumerate(secuence[init_player]['movimientos']):
-        hit = secuence[init_player]['golpes'][index]
-        special_hit =  movement_special(
-            movement + hit, skills[init_player]['special']
-        )
+    index = 0
+    while skills[init_player]['points'] > 0 and skills[end_player]['points']  > 0:
 
-        if special_hit:
-            points_p2 -= special_hit[1]
-            print(f"{skills[init_player]['name']} conecta un {special_hit[0]}")
-        if not hit:
-            print(f"{skills[init_player]['name']} se mueve")
-        if hit and not movement:
-            points_p2 -= 1
-            print(f"{skills[init_player]['name']} lanza una {skills[init_player]['basic'][hit][0]}")
-        if not special_hit and hit and movement:
-            points_p2 -= 1
-            print(f"{skills[init_player]['name']} se mueve y lanza un(a) {skills[init_player]['basic'][hit][0]}")
-        if points_p2 <= 0:
-            return print(f"{skills[init_player]['name']} gana la pelea y aun le queda {points_p2}")
+        if len(secuence[init_player]['movimientos']) > index:
+            remaining_points_ip = printer_events(
+                secuence[init_player]['movimientos'][index],
+                secuence[init_player]['golpes'][index],
+                skills[init_player]
+            )
 
+            skills[end_player]['points'] -= remaining_points_ip
 
-        movement = secuence[end_player]['movimientos'][index]
-        hit = secuence[end_player]['golpes'][index]
-        special_hit =  movement_special(
-            movement + hit, skills[end_player]['special']
-        )
+        if len(secuence[end_player]['movimientos']) > index:
+            remaining_points_ep = printer_events(
+                secuence[end_player]['movimientos'][index],
+                secuence[end_player]['golpes'][index],
+                skills[end_player]
+            )
+            skills[init_player]['points'] -= remaining_points_ep
 
-        if special_hit:
-            points_p1 -= special_hit[1]
-            print(f"{skills[end_player]['name']} conecta un {special_hit[0]}")
-        if not hit:
-            print(f"{skills[end_player]['name']} se mueve")
-        if hit and not movement:
-            points_p1 -= 1
-            print(f"{skills[end_player]['name']} lanza una {skills[end_player]['basic'][hit][0]}")
-        if not special_hit and hit and movement:
-            points_p1 -= 1
-            print(f"{skills[end_player]['name']} se mueve y lanza un(a) {skills[end_player]['basic'][hit][0]}")
-        if points_p1 <= 0:
-            return print((f"{skills[end_player]['name']} gana la pelea y aun le queda {points_p2}"))
+        index += 1
 
-secuence = input()
-printer_events(secuence)
+    if skills[init_player]['points'] <= 0:
+        return print(f"{skills[end_player]['name']} gana la pelea y aun le queda {skills[end_player]['points']} de energía")
+    else:
+        return print(f"{skills[init_player]['name']} gana la pelea y aun le queda {skills[init_player]['points']} de energía")
+
+secuence = input("Ingrese la secuencia de combate: ")
+talana_kombat(secuence)
